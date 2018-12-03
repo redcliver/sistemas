c1270acafed0d748e5b310eebbedd0d00c3f3866
Igor <igor-peres@hotmail.com>
2018-12-03 13:25:31 -0300

    1.01

--- chica_estoque/urls.py
+++ chica_estoque/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^entrada', views.entrada),
     url(r'^saida', views.saida),
+    url(r'^nova_saida', views.nova_saida),
     url(r'^nova_entrada', views.nova_entrada),
     url(r'^novo_produto', views.novo_produto),
     url(r'^consulta', views.consulta),

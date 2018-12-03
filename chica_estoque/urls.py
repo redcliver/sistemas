4918f91e6cc400a4595fa2785b08c2e54b831155
Igor <igor-peres@hotmail.com>
2018-12-03 12:19:10 -0300

    1.01
    consultar estoque chica

--- chica_estoque/urls.py
+++ chica_estoque/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^entrada', views.entrada),
     url(r'^nova_entrada', views.nova_entrada),
     url(r'^novo_produto', views.novo_produto),
+    url(r'^consulta', views.consulta),
     ]
--- chica_estoque/views.py
+++ chica_estoque/views.py
@@ -30,7 +30,7 @@ def nova_entrada(request):
                 quantidade = request.POST.get('quantidade')
                 n_lote = lote(prod=produto_obj, valor_compra=valor_compra, valor_venda=valor_venda, quantidade=quantidade)
                 n_lote.save()
-                produto_obj.quantidade = produto_obj.quantidade + quantidade
+                produto_obj.quantidade = produto_obj.quantidade + Decimal(quantidade)
                 produto_obj.valor_compra = valor_compra
                 produto_obj.valor_venda = valor_venda
                 lucro = Decimal(valor_compra) - Decimal(valor_venda)
@@ -65,6 +65,20 @@ def novo_produto(request):
                 msg = novo_produto.nome + " cadastrado com suceso!"
                 return render(request, 'chica_estoque/estoque_produto.html', {'title':'Novo Produto', 'msg':msg})
             return render(request, 'chica_estoque/estoque_produto.html', {'title':'Novo Produto'})
+        return render(request, 'sistema_login/erro.html', {'title':'Erro'})
+    else:
+        return render(request, 'sistema_login/erro.html', {'title':'Erro'})
+
+def consulta(request):
+    if request.user.is_authenticated():
+        empresa = request.user.get_short_name()
+        if empresa == 'chicadiniz':
+            produtos = produto.objects.all().order_by('nome')
+            if request.method == 'POST' and request.POST.get('produto_id') != None:
+                produto_id = request.POST.get('produto_id')
+                produto_obj = produto.objects.filter(id=produto_id).get()
+                return render(request, 'chica_estoque/estoque_consulta.html', {'title':'Consultar estoque', 'produto_obj':produto_obj})
+            return render(request, 'chica_estoque/estoque_consulta.html', {'title':'Consultar estoque', 'produtos':produtos})
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})
     else:
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})

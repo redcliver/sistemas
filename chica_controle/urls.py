975944f8a6c032dc2a0aa59210bb45dd8a50fa53
Igor <igor-peres@hotmail.com>
2018-12-04 12:17:38 -0300

    1.01
    configurado as url

--- chica_controle/urls.py
+++ chica_controle/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     url(r'^busca_funcionario', views.busca_funcionario),
     url(r'^edita_funcionario', views.edita_funcionario),
     url(r'^salva_funcionario', views.salva_funcionario),
+    url(r'^novo_servico', views.novo_servico),
+    url(r'^busca_servico', views.busca_servico),
+    url(r'^edita_servico', views.edita_servico),
+    url(r'^salva_servico', views.salva_servico),
     ]

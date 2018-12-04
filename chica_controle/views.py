8d89e78e52b2c7a3b11a5431d36eedb0f7f237af
Igor <igor-peres@hotmail.com>
2018-12-04 10:51:14 -0300

    1.01
    separando controle

--- chica_controle/views.py
+++ chica_controle/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
 def chica_controle(request):
     if request.user.is_authenticated():
         empresa = request.user.get_short_name()
+        cargo = request.user.last_name
         if empresa == 'chicadiniz':
-            return render(request, 'chica_controle/controle.html', {'title':'Controle'})
+            if cargo == 'boss':
+                return render(request, 'chica_controle/controle.html', {'title':'Controle'})
+            return render(request, 'chica_home/home.html', {'title':'Home'})
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})
     else:
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})

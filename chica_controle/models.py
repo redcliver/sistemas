50637953a93db3c683557bc0ede30c65b382694b
igor <igor-peres@hotmail.com>
2018-12-04 12:42:28 -0300

    1.01

--- chica_controle/models.py
+++ chica_controle/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class funcionario(models.Model):
     id = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=200)
+    nome = models.CharField(max_length=210)
     data_nasc = models.DateTimeField(null=True, blank=True)
     telefone = models.CharField(max_length=20, null=True, blank=True)
     celular = models.CharField(max_length=20, null=True, blank=True)
@@ -18,7 +18,7 @@ class servico(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    descricao = models.CharField(max_length=200, null=True, blank=True)
+    descricao = models.CharField(max_length=210, null=True, blank=True)
     data_cadastro = models.DateTimeField(default=timezone.now)
     
     def __str__(self):

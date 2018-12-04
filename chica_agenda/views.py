b68bd5f63e4bad50879d74864318bab943401288
Igor <igor-peres@hotmail.com>
2018-12-04 14:56:41 -0300

    1.01

--- chica_agenda/views.py
+++ chica_agenda/views.py
@@ -66,6 +66,7 @@ def visualiza(request):
             hoje = datetime.date.today().strftime('%Y-%m-%d')
             if request.method == 'POST' and request.POST.get('data') != None:
                 data = request.POST.get('data')
+                data = data.strftime('%Y-%m-%d')
                 agendas = agenda.objects.filter(data__icontains=data).all()
                 return render(request, 'chica_agenda/agenda_visualiza.html', {'title':'Visualizar Agenda', 'agendas':agendas, 'hoje':data})
             return render(request, 'chica_agenda/agenda_visualiza.html', {'title':'Visualizar Agenda', 'agendas':agendas, 'hoje':hoje})

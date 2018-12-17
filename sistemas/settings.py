7c92a004c2d100d9077b826236d04bac474300d0
redcliver <igor-peres@hotmail.com>
2018-12-17 15:04:47 -0300

    Update settings.py

--- sistemas/settings.py
+++ sistemas/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import posixpath
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,7 +116,7 @@ DATABASES = {
     }
 }
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
@@ -162,4 +162,4 @@ STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 LOGIN_REDIRECT_URL = '/sistema_login'
 
 # Activate Django-Heroku.
-#django_heroku.settings(locals())
+django_heroku.settings(locals())

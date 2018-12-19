86ade2e14d2f9035f3133b94053f9a9471a13586
Igor <igor-peres@hotmail.com>
2018-12-19 18:59:56 -0300

    2.02
    alteração no cadastro de cliente

--- lavajato_cliente/models.py
+++ lavajato_cliente/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class carro(models.Model):
+    id = models.AutoField(primary_key=True)
+    modelo = models.CharField(max_length=200)
+    placa = models.CharField(max_length=20, null=True, blank=True)
+    cor = models.CharField(max_length=100, null=True, blank=True)
+    observacao = models.CharField(max_length=300, null=True, blank=True)
+    data_cadastro = models.DateTimeField(default=timezone.now)
+    
+    def __str__(self):
+        return self.nome
+
 class cliente(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=200)
@@ -9,6 +20,11 @@ class cliente(models.Model):
     telefone = models.CharField(max_length=20, null=True, blank=True)
     celular = models.CharField(max_length=20, null=True, blank=True)
     email = models.CharField(max_length=200, null=True, blank=True)
+    endereco = models.CharField(max_length=200, null=True, blank=True)
+    numero = models.CharField(max_length=200, null=True, blank=True)
+    bairro = models.CharField(max_length=200, null=True, blank=True)
+    cidade = models.CharField(max_length=200, null=True, blank=True)
+    carros = models.ManyToManyField(carro)
     data_cadastro = models.DateTimeField(default=timezone.now)
     
     def __str__(self):

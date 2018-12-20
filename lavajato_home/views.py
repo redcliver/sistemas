d44b4f607523991716fecc7cb3436ea2920b3db3
Igor <igor-peres@hotmail.com>
2018-12-19 21:01:27 -0300

    2.03
    adicionado quantidade minima no estoque

--- lavajato_home/views.py
+++ lavajato_home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from lavajato_contas.models import conta
+from lavajato_estoque.models import produto
 from django.utils import timezone
 import datetime
 
@@ -10,9 +11,13 @@ def home(request):
         if empresa == 'dayson':
             hoje = datetime.date.today().strftime('%Y-%m-%d')
             vencimento_conta = 0
+            estoque_min = 0
             for c in conta.objects.filter(estado=1, data_venc__icontains=hoje).all():
                 vencimento_conta = vencimento_conta + 1
-            return render(request, 'lavajato_home/home.html', {'title':'Home', 'vencimento_conta':vencimento_conta})
+            for p in produto.objects.all():
+                if p.quantidade <= p.quantidade_minima or p.quantidade == p.quantidade_minima:
+                    estoque_min = estoque_min + 1
+            return render(request, 'lavajato_home/home.html', {'title':'Home', 'vencimento_conta':vencimento_conta, 'estoque_min':estoque_min})
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})
     else:
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})

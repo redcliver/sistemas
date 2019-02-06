cc3e64957a18fe0566b6b5a2ced1fa38e1a1356f
Igor <igor-peres@hotmail.com>
2019-02-06 11:23:45 -0300

    ajuste nova ordem

--- lavajato_agenda/views.py
+++ lavajato_agenda/views.py
@@ -48,7 +48,8 @@ def novo(request):
                 clientes = cliente.objects.all().order_by('nome')
                 servicos = servico.objects.all().order_by('nome')
                 funcionarios = funcionario.objects.all().order_by('nome')
-                return render(request, 'lavajato_agenda/agenda_add_servico.html', {'title':'Novo Servico', 'msg':msg, 'clientes':clientes, 'servicos':servicos, 'funcionarios':funcionarios})
+                it_servicos = novo_agendamento.item_servico.all()
+                return render(request, 'lavajato_agenda/agenda_add_servico.html', {'title':'Novo Servico', 'agenda_obj':novo_agendamento, 'it_servicos':it_servicos, 'msg':msg, 'clientes':clientes, 'servicos':servicos, 'funcionarios':funcionarios})
             return render(request, 'lavajato_agenda/agenda_novo.html', {'title':'Novo Agendamento', 'clientes':clientes, 'servicos':servicos, 'funcionarios':funcionarios, 'cliente_obj':cliente_obj})
         return render(request, 'sistema_login/erro.html', {'title':'Erro'})
     else:
